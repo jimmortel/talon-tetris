24b293bf803f89815ef1ea8379e6e7d0273f9ce3
jimmortel <jimmp2010@gmail.com>
2026-06-03 22:18:57 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import Tetris from '../components/Tetris';
+
 export default function Home() {
   return (
-    <main style={{ padding: '20px', textAlign: 'center' }}>
-      <h1>TALON Tetris est en construction</h1>
+    <main>
+      <Tetris />
     </main>
   );
 }
